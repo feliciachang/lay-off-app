3dd686e0bc28d19d3b5f65ed6b8cc844c4c8dadd
Felicia <ffeliciachang@gmail.com>
2023-01-11 16:15:03 -0800

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,12 @@ export default function App() {
       <Messages />
       <p>
         laid off over zoom. designed the weekend after. find felicia on{' '}
-        <a href="https://twitter.com/felchang">twitter</a>.
+        <a href="https://twitter.com/felchang">twitter</a>. lets talk irl!
+      </p>
+      <p>
+        or join the
+        <a href="https://discord.gg/mUyBDV325e">laid off club discord </a> i
+        just made lol
       </p>
     </main>
   )
