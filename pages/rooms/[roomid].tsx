6a4cb515002dc31dac5bde74e05f1a543a27b0d8
Felicia <ffeliciachang@gmail.com>
2023-01-13 18:02:51 -0800

    wip

--- pages/rooms/[roomid].tsx
+++ pages/rooms/[roomid].tsx
@@ -16,13 +16,7 @@ export default function Room() {
       <Messages roomId={getPageId(roomid)} />
       <p>
         laid off over zoom. designed the weekend after. find felicia on{' '}
-        <a href="https://twitter.com/felchang">twitter</a>. lets talk irl!
-      </p>
-      <p>
-        or join the
-        <a href="https://discord.gg/mUyBDV325e">laid off club discord </a> i
-        just made lol. also wana make my own chat room for the fun of it. stay
-        tuned.
+        <a href="https://twitter.com/felchang">twitter</a>.
       </p>
     </main>
   )
