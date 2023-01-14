f88f31b20ffb04b8625ffdc86a09d88377567036
Felicia <ffeliciachang@gmail.com>
2023-01-14 11:49:22 -0800

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,11 @@ export default function App() {
       <Messages />
       <p>
         laid off over zoom. designed the weekend after. find felicia on{' '}
-        <a href="https://twitter.com/felchang">twitter</a>.
+        <a href="https://twitter.com/felchang">twitter</a>.{' '}
+        <a href="https://laid-off-club.vercel.app/rooms/8OYxCORV3mR4qwQuVCFLqw">
+          about this site
+        </a>
+        .
       </p>
     </main>
   )
