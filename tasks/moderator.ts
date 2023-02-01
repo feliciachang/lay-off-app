4f215d3f13b0b1ae0de086d429c36c230165d5bf
Sarim Abbas <sarim.stays@gmail.com>
2023-01-31 19:53:32 -0800

    wip

--- tasks/moderator.ts
+++ tasks/moderator.ts
@@ -20,7 +20,7 @@ export const moderator = Scheduler<IModeratorJobPayload, IModeratorJobResponse>(
       baseUrl:
         process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000'
-          : process.env.VERCEL_URL,
+          : `https://${process.env.VERCEL_URL}`,
     },
   }
 )
