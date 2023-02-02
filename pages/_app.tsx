3b6b010e72eebc0c4ac3d1af00f2579c2a4a81d4
Sarim Abbas <sarim.stays@gmail.com>
2023-02-01 22:09:42 -0800

    flag guard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,24 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import clientConfig from '../convex/_generated/clientConfig'
-import { CursorRenderer } from '../utils/cursors'
+import { CursorRenderer } from '../components/cursors'
+import { useFlags } from 'flags/client'
+
 const convex = new ConvexReactClient(clientConfig)
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const flags = useFlags()
+  const ffInteractiveCursors = flags.data?.flags['interactive-cursors']
+
   return (
     <ConvexProvider client={convex}>
-      <CursorRenderer>
+      {ffInteractiveCursors ? (
+        <CursorRenderer>
+          <Component {...pageProps} />
+        </CursorRenderer>
+      ) : (
         <Component {...pageProps} />
-      </CursorRenderer>
+      )}
       <Analytics />
     </ConvexProvider>
   )
