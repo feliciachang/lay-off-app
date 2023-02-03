9ff2d055554a099a6b1a2796954172af55c98a7b
Felicia <ffeliciachang@gmail.com>
2023-02-02 21:32:49 -0800

    lol

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+}
+
+module.exports = nextConfig
