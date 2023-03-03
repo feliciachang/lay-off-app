efe5cc1c3886619492758789d8d9ec6946410cad
Felicia <ffeliciachang@gmail.com>
2023-03-03 13:22:24 -0800

    wip

--- components/messages/message-stream.tsx
+++ components/messages/message-stream.tsx
@@ -7,6 +7,7 @@ import { useUser } from '@clerk/clerk-react'
 import styles from './message-stream.module.css'
 import formStyles from '../emails/form.module.css'
 import cx from 'classnames'
+import sendSubresponse from '../../convex/sendSubresponse'
 
 interface MessageStreamProps {
   id: string
@@ -51,12 +52,12 @@ export default function MessageStream(props: MessageStreamProps) {
               reset()
             })}
           >
-            <textarea
+            <input
               className={formStyles.formInput}
               placeholder="join the club, add a reply"
               {...register('newResponseText', { required: true })}
             />
-            <textarea
+            <input
               className={cx(formStyles.formInput, formStyles.addMargin)}
               placeholder="and a url, if necessary"
               {...register('newResponseUrl', {
@@ -116,6 +117,65 @@ export default function MessageStream(props: MessageStreamProps) {
   )
 }
 
+interface SubreponseFormProps {
+  id: string
+}
+
+export function SubresponseForm(props: SubreponseFormProps) {
+  const { id } = props
+  const sendSubresponse = useMutation('sendResponse')
+
+  const { user } = useUser()
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm()
+
+  return (
+    <form
+      className={formStyles.messageForm}
+      onSubmit={handleSubmit(async (data) => {
+        await sendSubresponse(
+          id,
+          data.newSubresponseText,
+          user?.id || '',
+          data.newSubresponseUrl
+        )
+        reset()
+      })}
+    >
+      <input
+        className={formStyles.formInput}
+        placeholder="start a mini convo"
+        {...register('newSubresponseText', { required: true })}
+      />
+      <input
+        className={cx(formStyles.formInput, formStyles.addMargin)}
+        placeholder="and a url, if necessary"
+        {...register('newSubresponseUrl', {
+          pattern: {
+            value: new RegExp(
+              '^(https?:\\/\\/)?' + // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?$',
+              'i'
+            ),
+            message: 'invalid url',
+          },
+        })}
+      />
+      <button className={formStyles.submitButton} type="submit">
+        <Image src="/arrow.svg" alt="arrow" width={15} height={15} />
+      </button>
+    </form>
+  )
+}
 interface ResponseBodyProps {
   body: string
   url: string
@@ -144,6 +204,7 @@ export function ResponseBody(props: ResponseBodyProps) {
   const maxTextLenRef = useRef(250)
   const [toggleTextLen, setToggleTextLen] = useState(false)
   const [showReadMore, setShowReadMore] = useState(false)
+  const [isHovering, setIsHovering] = useState(false)
 
   useEffect(() => {
     if (body.length <= maxTextLenRef.current) return
@@ -180,32 +241,58 @@ export function ResponseBody(props: ResponseBodyProps) {
   }
   return (
     <div
-      className={styles.responseContainer}
-      style={{ animationDelay: animStr(idx) }}
+      className={styles.responseAndSubresponses}
+      onMouseOver={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
     >
-      <span>
-        {message}
-        {showReadMore && (
-          <a
-            className={styles.readMore}
-            onClick={(): void => setToggleTextLen(!toggleTextLen)}
-          >
-            {toggleTextLen ? ' read more' : ' read less'}
-          </a>
+      <div
+        className={styles.responseContainer}
+        style={{ animationDelay: animStr(idx) }}
+      >
+        <span>
+          {message}
+          {showReadMore && (
+            <a
+              className={styles.readMore}
+              onClick={(): void => setToggleTextLen(!toggleTextLen)}
+            >
+              {toggleTextLen ? ' read more' : ' read less'}
+            </a>
+          )}
+        </span>
+        {url?.length > 0 && (
+          <Image
+            className={styles.urlArrow}
+            src="/arrow.svg"
+            alt="arrow"
+            width={15}
+            height={15}
+          />
         )}
-      </span>
-      {url?.length > 0 && (
-        <Image
-          className={styles.urlArrow}
-          src="/arrow.svg"
-          alt="arrow"
-          width={15}
-          height={15}
-        />
-      )}
+      </div>
       {subresponses?.map((subresponse) => {
-        return <div>{subresponse.body}</div>
+        return (
+          <div
+            style={{
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              textOverflow: 'clip',
+              fontSize: '18px',
+              display: 'flex',
+              color: '#D0D0D0',
+            }}
+          >
+            <Image
+              src="/dropdown-arrow.svg"
+              alt="arrow"
+              width={15}
+              height={15}
+            />
+            {subresponse.body}
+          </div>
+        )
       })}
+      {isHovering && <SubresponseForm id={id} />}
     </div>
   )
 }
--- convex/listSubresponses.ts
+++ convex/listSubresponses.ts
@@ -2,5 +2,5 @@ import { query } from './_generated/server'
 import { Document } from './_generated/dataModel'
 
 export default query(async ({ db }, messageId): Promise<Document<'responses'>[]> => {
-  return await db.query('responses').order("desc").filter((q) => q.eq(q.field("messageId"), messageId)).collect()
+  return await db.query('responses').order("asc").filter((q) => q.eq(q.field("messageId"), messageId)).collect()
 })
