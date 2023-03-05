f0e2ad0c5a9b7788b806ca2a4468eebf7b667dd0
Felicia <ffeliciachang@gmail.com>
2023-03-04 19:23:18 -0800

    wip

--- components/responses/subresponse.tsx
+++ components/responses/subresponse.tsx
@@ -1,8 +1,4 @@
 import Image from 'next/image'
-import { useForm } from 'react-hook-form'
-import { useUser } from '@clerk/clerk-react'
-import formStyles from '../emails/form.module.css'
-import { useMutation } from '../../convex/_generated/react'
 import { formatURL } from '../../utils'
 import useReadMore from '../messages/use-read-more'
 import styles from './index.module.css'
@@ -64,65 +60,5 @@ export default function SubresponseBody(
         )}
       </span>
     </div>
-  )
-}
-
-interface SubreponseFormProps {
-  id: string
-}
-
-export function SubresponseForm(props: SubreponseFormProps) {
-  const { id } = props
-  const sendSubresponse = useMutation('sendSubresponse')
-
-  const { user } = useUser()
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm()
-
-  return (
-    <form
-      className={formStyles.messageForm}
-      onSubmit={handleSubmit(async (data) => {
-        await sendSubresponse(
-          id,
-          data.newSubresponseText,
-          user?.id || '',
-          data.newSubresponseUrl
-        )
-        reset()
-      })}
-    >
-      <input
-        className={formStyles.formInput}
-        placeholder="start a mini convo"
-        {...register('newSubresponseText', { required: true })}
-      />
-      <input
-        className={cx(formStyles.formInput, formStyles.addMargin)}
-        placeholder="and a url, if necessary"
-        {...register('newSubresponseUrl', {
-          pattern: {
-            value: new RegExp(
-              '^(https?:\\/\\/)?' + // protocol
-                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-                '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-                '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-                '(\\#[-a-z\\d_]*)?$',
-              'i'
-            ),
-            message: 'invalid url',
-          },
-        })}
-      />
-      <button className={formStyles.submitButton} type="submit">
-        <Image src="/arrow.svg" alt="arrow" width={15} height={15} />
-      </button>
-    </form>
   )
 }
