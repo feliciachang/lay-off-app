0e5e599c7c9b456efb712b89e031986d0a7a9bc1
Felicia <ffeliciachang@gmail.com>
2023-03-30 08:43:56 +0800

    wip

--- convex/listResponses.ts
+++ convex/listResponses.ts
@@ -1,6 +1,12 @@
 import { query } from './_generated/server'
 import { Document } from './_generated/dataModel'
 
-export default query(async ({ db }, messageId): Promise<Document<'responses'>[]> => {
-  return await db.query('responses').order("desc").filter((q) => q.eq(q.field("messageId"), messageId)).collect()
-})
+export default query(
+  async ({ db }, messageId): Promise<Document<'responses'>[]> => {
+    return await db
+      .query('responses')
+      .order('desc')
+      .filter((q) => q.eq(q.field('messageId'), messageId))
+      .collect()
+  }
+)
